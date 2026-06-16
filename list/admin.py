4eb0072d9cbe8be6f57feb9067e8a6ff149d7b6d
Maksym Pariichuk <maksim.pariychuk07@gmail.com>
2026-06-16 13:59:53 +0200

    feat: Add admin panel

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "done","get_tags")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related("tags")
+
+    def get_tags(self, obj):
+        return ", ".join(tag.name for tag in obj.tags.all())
+
+    get_tags.short_description = "Tags"
